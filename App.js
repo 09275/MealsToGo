dd96975ee23da77e8452ca8a5e521fb372181514
Panagiotis Doukas <doukas@cloudevo.ai>
2023-01-18 16:09:08 +0200

    Add the Searchbar from react-native-paper

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import {StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import React from 'react';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { Searchbar } from "react-native-paper";
 import {
   StatusBar, 
   StyleSheet, 
   SafeAreaView, 
   Text, 
   View 
-} from 'react-native';
+} from "react-native";
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.search}>
-          <Text>search</Text>
+          <Searchbar placeholder="Search"/>
         </View>
         <View style={styles.list}>
           <Text>list</Text>
